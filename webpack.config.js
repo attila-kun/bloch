135ed1a4a17a0ccc490c69c66462c9e9cbfc310c
Attila Kun <kun.attila.hun@gmail.com>
2020-11-26 21:58:36 +0000

    CleanWebpackPlugin, HtmlWebpackPlugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
