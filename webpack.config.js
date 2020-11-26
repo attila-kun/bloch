207eb2d157a757082b6c3d6f9d429002cfdf92d7
Attila Kun <kun.attila.hun@gmail.com>
2020-11-26 22:50:54 +0000

    Added TypeScript.

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,19 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.ts',
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -15,6 +24,9 @@ module.exports = {
       title: 'Development',
     }),
   ],
+  resolve: {
+    extensions: ['.ts', '.js' ],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
