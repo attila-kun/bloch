402bebac3513e8edab9f9267369da9121ae39cce
Attila Kun <kun.attila.hun@gmail.com>
2020-12-05 21:12:56 +0000

    Print both ratios when calculating eigenvectors.

--- src/eigen.ts
+++ src/eigen.ts
@@ -2,7 +2,11 @@ import * as  mathjs from 'mathjs';
 
 export function calculateEigenVectors() {
 
-    const matrix = [[mathjs.complex(1, 6), 15], [7, -2]];
+    // const matrix = [[1, 0], [0, 1]];
+    // const matrix = [[0, mathjs.complex(0, -1)], [mathjs.complex(0, 1), 1]];
+    const matrix = [[mathjs.complex(1, 6), 15], [7, mathjs.complex(-2, -5)]];
+    // const matrix = mathjs.multiply(mathjs.sqrt(1/2), [[1, 1], [1, -1]]);
+    // console.log('matrix', matrix);
     const m00 = matrix[0][0];
     const m01 = matrix[0][1];
     const m10 = matrix[1][0];
@@ -18,22 +22,33 @@ export function calculateEigenVectors() {
         return mathjs.divide(mathjs.add(mathjs.multiply(-1, b), dWithFactor), mathjs.multiply(2, a));
     }
 
-    const ratio1: mathjs.Complex = calculateRatio(1) as mathjs.Complex;
-    const ratio2: mathjs.Complex = calculateRatio(-1) as mathjs.Complex;
+    const ratio1: mathjs.Complex = mathjs.complex(calculateRatio(1) as any);
+    const ratio2: mathjs.Complex = mathjs.complex(calculateRatio(-1) as any);
 
-    const polar: mathjs.PolarCoordinates = ratio1.toPolar();
+    const calculateEigenVector = (ratio: mathjs.Complex) => {
+        const polar: mathjs.PolarCoordinates = ratio1.toPolar();
+        const phase = mathjs.exp(mathjs.complex(0, polar.phi));
+        const theta = mathjs.atan(polar.r); // TODO: should atan2 be used here?
+        const x = mathjs.cos(theta);
+        const y = mathjs.multiply(mathjs.sin(theta), phase);
+        return [x, y];
+    };
 
-    const phase = mathjs.exp(mathjs.complex(0, polar.phi));
-    const theta = mathjs.atan(polar.r);
-    const x = mathjs.cos(theta);
-    const y = mathjs.multiply(mathjs.sin(theta), phase);
+    {
+        const ratios = {ratio1, ratio2};
+        for (let key in ratios) {
+            const ratio = ratios[key];
+            console.log('ratio:', ratio);
+            const [x, y] = calculateEigenVector(ratio);
+            const product = mathjs.multiply(mathjs.matrix(matrix as any), [x as any, y as any]);
 
-    const product = mathjs.multiply(mathjs.matrix(matrix as any), [x as any, y as any]);
+            console.log('product:', product);
+            console.log('x:', x);
+            console.log('y:', y);
 
-    console.log('product:', product);
-    console.log('x:', x);
-    console.log('y:', y);
+            console.log('x ratio:', mathjs.divide(product.get([0]), x));
+            console.log('y ratio:', mathjs.divide(product.get([1]), y));
+        }
+    }
 
-    console.log('x ratio:', mathjs.divide(product.get([0]), x));
-    console.log('y ratio:', mathjs.divide(product.get([1]), y));
 };
