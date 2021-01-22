da3ab6b8e047a239f2f5729a9b88b8f6c8786ca0
Attila Kun <kun.attila.hun@gmail.com>
2021-01-22 01:59:46 +0000

    Using flexbox for resizing.

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,22 @@ window.onload = function() {
   );
 
   function resizeCanvas() {
-    const canvasContainer = document.getElementById("canvasContainer");
-    bloch.resizeCanvas(canvasContainer.clientHeight);
+    const container = document.getElementById("container");
+    const settings = document.getElementById("settings");
+    const canvasRoot = document.getElementById("canvasRoot");
+
+    const containerRect = container.getBoundingClientRect();
+    const settingsRect = settings.getBoundingClientRect();
+    const canvasRootRect = canvasRoot.getBoundingClientRect();
+
+    if (settingsRect.top < canvasRootRect.top) { // wrapped
+      canvasRoot.style.height = String(containerRect.height - settingsRect.height) + 'px';
+    } else { // not wrapped, settings and canvas are next to each other
+      canvasRoot.style.height = null;
+    }
+
+    const size = Math.min(canvasRoot.clientWidth, canvasRoot.clientHeight);
+    bloch.resizeCanvas(size);
   }
 
   window.onresize = resizeCanvas;
